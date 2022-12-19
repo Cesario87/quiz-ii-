87926e1151d5bb41c23aac94f5c3e5f04689dcda
Cesario87 <ces.albred@gmail.com>
2022-12-19 21:11:17 +0100

    Created first form

--- preguntas.js
+++ preguntas.js
@@ -1,25 +1,42 @@
 async function printQuestions() {
-    let resultado = await fetch("https://opentdb.com/api.php?amount=10");
+    let resultado = await fetch("https://opentdb.com/api.php?amount=1");
     let dataBase = await resultado.json();
     const preguntas = dataBase.results;
     const arrPreguntas = preguntas.map(element => element.question);
-    
-   // window.prueba.innerHTML = arrPreguntas;
-    
+    const arrCorrectas = preguntas.map(element => element.correct_answer);
+    const arrIncorrectas = preguntas.map(element => element.incorrect_answers);
+
     const questions = [
         {
             "question": arrPreguntas,
-        
+            "correcta": arrCorrectas,
+            "incorrectas": arrIncorrectas
         }
     ]
+    
     for (let i = 0; i < arrPreguntas.length; i++) {
+        let arrMezcla = [arrCorrectas[i], ...arrIncorrectas[i]];
+        let shuffledArray = arrMezcla.sort(() => Math.random() - 0.5);
         const division = document.createElement("div");
         document.getElementById("prueba").appendChild(division);
-        division.innerHTML = questions[0].question[i];
+        division.setAttribute("id", "espacioPregunta");
+        const etiqueta = document.createElement("legend");
+        document.getElementById("espacioPregunta").appendChild(etiqueta);
+        etiqueta.innerHTML = questions[i].question[i];
+
+        for (let j = 0; j < arrMezcla.length; j++) {
+        const division2 = document.createElement("div");
+        document.getElementById("espacioPregunta").appendChild(division2);
+        division2.id = `respuesta${[j]}`;
+        const opciones = document.createElement("label");
+        document.getElementById(`respuesta${[j]}`).appendChild(opciones);
+        opciones.innerHTML = shuffledArray[j];
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        division2.appendChild(radio);
+
     }
     
 }
+}
 printQuestions()
-
-
-
