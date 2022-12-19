c31fb1b9ad78e31f531531cf64d4c43654b03b05
Cesario87 <ces.albred@gmail.com>
2022-12-19 21:39:21 +0100

    fix/

--- preguntas.js
+++ preguntas.js
@@ -2,27 +2,26 @@ async function printQuestions() {
     let resultado = await fetch("https://opentdb.com/api.php?amount=1");
     let dataBase = await resultado.json();
     const preguntas = dataBase.results;
-    const arrPreguntas = preguntas.map(element => element.question);
-    const arrCorrectas = preguntas.map(element => element.correct_answer);
+    const pregunta = preguntas.map(element => element.question);
+    const correcta = preguntas.map(element => element.correct_answer);
     const arrIncorrectas = preguntas.map(element => element.incorrect_answers);
 
     const questions = [
         {
-            "question": arrPreguntas,
-            "correcta": arrCorrectas,
-            "incorrectas": arrIncorrectas
+            "question": pregunta,
+            "correcta": correcta,
+            "incorrectas": arrIncorrectas,
         }
     ]
-    
-    for (let i = 0; i < arrPreguntas.length; i++) {
-        let arrMezcla = [arrCorrectas[i], ...arrIncorrectas[i]];
+
+        let arrMezcla = [...correcta, ...arrIncorrectas[0]];
         let shuffledArray = arrMezcla.sort(() => Math.random() - 0.5);
         const division = document.createElement("div");
         document.getElementById("prueba").appendChild(division);
         division.setAttribute("id", "espacioPregunta");
         const etiqueta = document.createElement("legend");
         document.getElementById("espacioPregunta").appendChild(etiqueta);
-        etiqueta.innerHTML = questions[i].question[i];
+        etiqueta.innerHTML = questions[0].question;
 
         for (let j = 0; j < arrMezcla.length; j++) {
         const division2 = document.createElement("div");
@@ -34,9 +33,7 @@ async function printQuestions() {
         const radio = document.createElement("input");
         radio.type = "radio";
         division2.appendChild(radio);
-
     }
-    
 }
-}
+
 printQuestions()
