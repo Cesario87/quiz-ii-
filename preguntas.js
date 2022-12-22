cb332e30627ed156b38a77b3c5157350e3737170
Cesario87 <ces.albred@gmail.com>
2022-12-22 21:25:37 +0100

    falta grafica y revisar css

--- preguntas.js
+++ preguntas.js
@@ -1,101 +1,117 @@
-async function getQuestions() {
-    let resultado = await fetch("https://opentdb.com/api.php?amount=10");
-    let dataBase = await resultado.json();
-    const preguntas = dataBase.results;
 
-    return preguntas;
-}
-
-async function init(){
-    let pregunta = await getQuestions()
-    let num = 0
-    nextQuestion(pregunta[num],num)
-    num = 1
-    console.log(pregunta);
-    document.querySelector('#btn').addEventListener('click',()=> {
-        nextQuestion(pregunta[num],num)
-        num++
-    })
-}
-
-init()
-async function nextQuestion(pregunta,num) {
-    
-    let espacio = document.querySelector('#espacioPregunta')
-    let opciones = document.querySelector('#opciones')
-    //console.log(num);
-
-    if(num < 10){
-    
-   espacio.innerHTML = `<div>
-   <legend id='legend${num}'>${pregunta.question}</legend>
-   </div>` 
-
-    let arrMezcla = []
-    correcta = [pregunta.correct_answer]
-    incorrectas = pregunta.incorrect_answers
-    arrMezcla = correcta.concat(incorrectas)
+if (document.title == 'Hoja de preguntas') {
   
-    mezclarArray(arrMezcla)
-    //console.log(arrMezcla);
-    
-    let imprimir2 = ''
-    
-    for (let j = 0; j < arrMezcla.length; j++) {
-        imprimir2 +=`<div>
-        <label for="radio${j}">${arrMezcla[j]}</label>
-        <input type="radio" id="radio${num+j}" name="${num}" value="${arrMezcla[j]}"> 
-        </div>` 
 
-    }
-
-    opciones.innerHTML = imprimir2
-
-    validar(pregunta,num)  
-  }else{
-    window.open("./results.html"); 
+  async function getQuestions() {
+      let resultado = await fetch("https://opentdb.com/api.php?amount=10");
+      let dataBase = await resultado.json();
+      const preguntas = dataBase.results;
+  
+      return preguntas;
   }
-}
-
-let puntuacion = 0
-
-function validar(pregunta,num) {
+  
+  async function init(){
+      let pregunta = await getQuestions()
+      let num = 0
+      nextQuestion(pregunta[num],num)
+      num = 1
+      console.log(pregunta);
+      document.querySelector('#btn').addEventListener('click',()=> {
+          nextQuestion(pregunta[num],num)
+          num++
+      })
+  }
+  
+  init()
+  async function nextQuestion(pregunta,num) {
+      
+      let espacio = document.querySelector('#espacioPregunta')
+      let opciones = document.querySelector('#opciones')
+      //console.log(num);
+  
+      if(num < 10){
+      
+     espacio.innerHTML = `<div>
+     <legend id='legend${num}'>${pregunta.question}</legend>
+     </div>` 
+  
+      let arrMezcla = []
+      correcta = [pregunta.correct_answer]
+      incorrectas = pregunta.incorrect_answers
+      arrMezcla = correcta.concat(incorrectas)
     
-        document.querySelector('#espacioTotal').addEventListener('change', function (event) {
-        
-        event.preventDefault()
-        console.log(event.target.value);
-        let counter = 0
-        
-        console.log("Estoy por "+num);
-        let selected = event.target.value;
-        
-        if (!selected) {
-            alert('Selecciona una opción')
-            
-        }else if (selected == pregunta.correct_answer) {
-            counter++ 
-            puntuacion =  puntuacion + counter
-            localStorage.setItem('puntuacion',puntuacion)
-        }
-        console.log(counter, '1');
-        console.log(puntuacion, 'puntuacion');
-    }) 
-    return puntuacion  
-} 
-
-function mezclarArray(arr) {
-    
-    for (let i = arr.length - 1; i >= 0; i--) {
-          const s = Math.floor(Math.random() * (i + 1));
-          [arr[i], arr[s]] = [arr[s], arr[i]];
-          
-        }
+      mezclarArray(arrMezcla)
+      //console.log(arrMezcla);
+      
+      let imprimir2 = ''
+      
+      for (let j = 0; j < arrMezcla.length; j++) {
+          imprimir2 +=`<div>
+          <label for="radio${j}">${arrMezcla[j]}</label>
+          <input type="radio" id="radio${num+j}" name="${num}" value="${arrMezcla[j]}"> 
+          </div>` 
+  
       }
-
-
-  let puntuacionTotal = localStorage.getItem('puntuacion')
-  document.getElementById('datosguardados').innerHTML =`<div>${puntuacionTotal}/10</div>`;
+  
+      opciones.innerHTML = imprimir2
+  
+      validar(pregunta,num)  
+    }else{
+      window.open("./results.html"); 
+    }
+  }
+  
+  let puntuacion = 0
+  let today = new Date().toLocaleDateString()
+  
+  function validar(pregunta,num) {
+    
+          document.querySelector('#espacioTotal').addEventListener('change', function (event) {
+          
+          event.preventDefault()
+          console.log(event.target.value);
+          let counter = 0
+          
+          console.log("Estoy por "+num);
+          let selected = event.target.value;
+          
+          if (!selected) {
+              alert('Selecciona una opción')
+              
+          }else if (selected == pregunta.correct_answer) {
+              counter++ 
+              puntuacion =  puntuacion + counter
+              
+  
+              //localStorage.setItem('puntuacion',puntuacion)
+              localStorage.setItem(
+                "user",
+                JSON.stringify({
+                  'score': puntuacion,
+                  'fecha': today,
+                })
+              );
+          }
+          console.log(counter, '1');
+          console.log(puntuacion, 'puntuacion');
+      }) 
+      return puntuacion  
+  } 
+  
+  function mezclarArray(arr) {
+      
+      for (let i = arr.length - 1; i >= 0; i--) {
+            const s = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[s]] = [arr[s], arr[i]];
+            
+          }
+        }
+  }
+  
+  if(document.title == 'Results'){
+    let puntuacionTotal = JSON.parse(localStorage.getItem("user"))
+    document.getElementById('datosguardados').innerHTML =`<div>${puntuacionTotal.score}/10</div>`;
+  }
 
 
 //GRÁFICA
